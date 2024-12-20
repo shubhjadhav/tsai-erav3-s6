e5dbafe20c30924e94fb7a25b1942379dcf4ebf0
shubhjadhav <shubhamjadhav1017@gmail.com>
2024-12-19 22:08:10 -0600

    Add additional test cases

--- tests/test_mnist.py
+++ tests/test_mnist.py
@@ -40,28 +40,66 @@ def test_has_dropout_layer():
     has_dropout_layer = False
     
     for layer in model.modules():
-        has_dropout_layer = has_dropout_layer or isinstance(layer, nn.Dropout)
+        if isinstance(layer, nn.Dropout):
+            has_dropout_layer = True
+            break
 
     try:
-        assert has_dropout_layer == False , "Model does not have drop out layer"
+        assert has_dropout_layer , "Model have drop out layer"
         logger.info("✓ Model Dropout Layer test passed")
     except AssertionError as e:
         logger.error(f"✗ Model Dropout Layer test failed: {str(e)}")
         raise
 
-def test_batch_norm():
+def test_has_batch_norm():
     logger.info("Starting model batch norm layer test")
     model = MNIST()
     has_batch_norm_layer = False
-    
+
     for layer in model.modules():
-        has_batch_norm_layer = has_batch_norm_layer or isinstance(layer, nn.BatchNorm2d)
+        if isinstance(layer, nn.BatchNorm2d):
+            has_batch_norm_layer = True
+            break
 
     try:
-        assert has_batch_norm_layer == False , "Model does not have batch normalization"
+        assert has_batch_norm_layer, "Model have batch normalization"
         logger.info("✓ Model Batch Norm test passed")
     except AssertionError as e:
         logger.error(f"✗ Model Batch Norm test failed: {str(e)}")
+        raise
+
+def test_has_gap():
+    logger.info("Starting model GAP layer test")
+    model = MNIST()
+    has_gap = False
+
+    for layer in model.modules():
+        if isinstance(layer, nn.AdaptiveAvgPool2d):
+            has_gap = True
+            break
+
+    try:
+        assert has_gap, "Model have GAP layer"
+        logger.info("✓ Model GAP Layer test passed")
+    except AssertionError as e:
+        logger.error(f"✗ Model GAP Layer test failed: {str(e)}")
+        raise
+
+def test_has_linear_layer():
+    logger.info("Starting model linear layer test")
+    model = MNIST()
+    has_linear_layer = False
+
+    for layer in model.modules():
+        if isinstance(layer, nn.Linear):
+            has_linear_layer = True
+            break
+
+    try:
+        assert has_linear_layer, "Model have linear layer"
+        logger.info("✓ Model Linear Layer test passed")
+    except AssertionError as e:
+        logger.error(f"✗ Model Linear Layer test failed: {str(e)}")
         raise
 
 def test_model_accuracy():
